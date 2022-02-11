24ce57a9465d2343b35e6609a0c5404b49a5893d
Sergey Obidin <player181285@gmail.com>
2022-02-11 15:07:09 +0400

    added allure, new test

--- src/test/java/ru/netology/CardDeliveryTest.java
+++ src/test/java/ru/netology/CardDeliveryTest.java
@@ -3,6 +3,14 @@ package ru.netology;
 
 import com.codeborne.selenide.Configuration;
 
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.Description;
+import io.qameta.allure.Feature;
+import io.qameta.allure.Step;
+import io.qameta.allure.Story;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.Keys;
@@ -15,18 +23,49 @@ import static com.codeborne.selenide.Selectors.withText;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
+
+@Feature("Заказ карты")
 public class CardDeliveryTest {
 
+    @BeforeAll
+    static void setUpAll() {
+        SelenideLogger.addListener("allure", new AllureSelenide());
+    }
 
-
+    @AfterAll
+    static void tearDownAll() {
+        SelenideLogger.removeListener("allure");
+    }
     @BeforeEach
-    void setUpAll() {
+    void setUpAllTest() {
         Configuration.holdBrowserOpen = true;
         open("http://localhost:9999");
     }
+
+    @Story("Заказ карты на дату через пять дней от сегодня")
+    @Test
+    public void shouldSendFullForm() {
+        CardInfoForRegistration infoForReg = DataGenerator.Registration.generateInfo("ru");
+        $("[data-test-id='city'] input").setValue(infoForReg.getCity());
+        $("[data-test-id='date'] input").doubleClick().sendKeys(Keys.DELETE);
+        $("[data-test-id='date'] input").setValue(DataGenerator.date(5));
+//           есть еще вариант поместить дату в переменную например
+//            String dateOfMeeting = DataGenerator.date(5);
+//            а потом подставлять ее там где нам необходимо, так же со второй датой
+        $("[data-test-id='name'] input").setValue(infoForReg.getName());
+        $(byName("phone")).setValue(infoForReg.getPhone()); // поиск по имени
+        $("[data-test-id='agreement']").click();
+        $(withText("Запланировать")).click();
+        $("[data-test-id='success-notification']").shouldHave(text("Успешно!"), Duration.ofSeconds(15));
+        $(".notification__content").shouldBe(exactText("Встреча успешно запланирована на "
+                + DataGenerator.date(5)), Duration.ofSeconds(15));
+        $("[data-test-id='success-notification']").shouldBe(visible).find(".icon-button__text").click();
+    }
+
+
+    @Story("Заказ карты на дату через пять дней от сегодня и изменение даты на десять дней от сегодня")
         @Test
-        public void shouldSendFullForm() {
-//            установка первой даты
+        public void shouldSendFullFormNewDate() {
             CardInfoForRegistration infoForReg = DataGenerator.Registration.generateInfo("ru");
             $("[data-test-id='city'] input").setValue(infoForReg.getCity());
             $("[data-test-id='date'] input").doubleClick().sendKeys(Keys.DELETE);
--- src/test/java/ru/netology/DataGenerator.java
+++ src/test/java/ru/netology/DataGenerator.java
@@ -2,11 +2,13 @@ package ru.netology;
 
 import com.github.javafaker.Faker;
 
+import io.qameta.allure.Step;
 import lombok.experimental.UtilityClass;
 
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.util.Locale;
+
 
 @UtilityClass
 public class DataGenerator {
@@ -14,6 +16,7 @@ public class DataGenerator {
 
     @UtilityClass
     public static class Registration {
+        @Step ("Получаем данные клиента для оформления карты")
         public static CardInfoForRegistration generateInfo(String locale) {
             Faker faker = new Faker(new Locale(locale));
             return new CardInfoForRegistration(faker.address().cityName(),
@@ -21,7 +24,7 @@ public class DataGenerator {
                     faker.numerify("+79#########"));
         }
     }
-
+    @Step ("Получаем локальную дату")
     public String date (int quantityOfDays) {
         return LocalDate.now().plusDays(quantityOfDays).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
 
